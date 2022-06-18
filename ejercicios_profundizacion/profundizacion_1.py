34a9ae1c3a2d132966089636e633e7f5fd72d1f4
Javier <javisid@gmail.com>
2022-06-18 11:55:07 -0300

    Se agrega profundizacion_1.py

--- ejercicios_profundizacion/profundizacion_1.py
+++ ejercicios_profundizacion/profundizacion_1.py
@@ -28,6 +28,23 @@ E) Exponente/Potencia
   se ha realizado la operación
   ej: La suma entre 4.2 y 6.5 es 10.7
 '''
-
 print('¡Nuestra primera calculadora!')
 # Empezar aquí la resolución del ejercicio
+print('Ingrese el primer numero')
+numero_1 = float(input())
+print('Ingrese el segundo numero')
+numero_2 = float(input())
+
+SUMA = numero_1 + numero_2
+RESTA = numero_1 - numero_2
+DIVISION = numero_1 / numero_2
+MULTIPLICACION = numero_1 * numero_2
+DIVISION = numero_1 / numero_2
+POTENCIA = numero_1 ** numero_2
+
+
+print('La suma de los numeros ingresados es =',SUMA)
+print('La resta de los numeros ingresados es =',RESTA)
+print('La division de los numeros ingresados es =',DIVISION)
+print('El multiplo de los numeros ingresados es =', MULTIPLICACION)
+print('La potencia de los numeros ingresados es =',POTENCIA)
